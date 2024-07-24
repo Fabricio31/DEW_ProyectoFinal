b77db8aaaaad5a64cf4c7f9f83221a2cc615e703
Fabricio31 <fabriciobecerra18@gmail.com>
2024-07-24 00:22:26 -0500

    Version Final Turisticos.com

---
+++ Paginas/java.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$('.toggle').click(function(){
+	$('.formulario').animate({
+		height:"toggle",
+		'padding-top':'toggle',
+		'padding-bottom':'toggle',
+		opacity: 'toggle'
+		
+	},"slow");
+});
